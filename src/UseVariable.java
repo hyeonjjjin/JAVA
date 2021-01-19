f449ca186e38cfa26364e982bb8adf70218fb19a
hyeonjjjin <67496431+hyeonjjjin@users.noreply.github.com>
2021-01-19 23:19:47 +0900

    Add files via upload

---
+++ src/UseVariable.java
@@ -0,0 +1,18 @@
+class UseVariable {
+    public static void main(String[] args){
+        int num1;
+        num1 = 10;
+        int num2 = 20;
+        int num3 = num1+ num2;
+        System.out.println(num1+"+"+num2+"="+num3);
+
+        char ch1 = '헐';
+        char ch2 = '확';
+        System.out.println(ch1+" "+ch2);
+
+        boolean b1 = true;
+        boolean b2 = false;
+        System.out.println(num1<num2);
+        System.out.println(num1>num2);
+    }
+}
