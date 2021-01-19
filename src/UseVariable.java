3d5e12e390ea636307af32444fd5ab9c59fd9ac5
hjjja <jahj30734@gmail.com>
2021-01-19 22:44:37 +0900

    chap2_fin

--- src/UseVariable.java
+++ src/UseVariable.java
@@ -5,5 +5,14 @@ class UseVariable {
         int num2 = 20;
         int num3 = num1+ num2;
         System.out.println(num1+"+"+num2+"="+num3);
+
+        char ch1 = '헐';
+        char ch2 = '확';
+        System.out.println(ch1+" "+ch2);
+
+        boolean b1 = true;
+        boolean b2 = false;
+        System.out.println(num1<num2);
+        System.out.println(num1>num2);
     }
 }
