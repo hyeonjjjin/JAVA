580b1b9d460ac1c529f0a0079ecd525b9be57e4d
hjjja <jahj30734@gmail.com>
2021-02-01 23:52:56 +0900

    chapter7_fin

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,8 +1,16 @@
-//chapter 7-1. class 7-2. string
+//chapter 7-1. class 7-2. string 7-3 name_rule
+
+//class name: Camel Case 모델
+//첫 문자는 대문자로. 여러 단어가 사용된다면 새로운 단어의 시작은 대문자.
 public class BankAccount {
+    //메소드 및 변수: 변형된 Camel Case 모델
+    //첫 번째 단어의 첫 문자만 소문자로 함.
     String accNumber; //계좌번호 _구분할 수 있는 고유의 값이 있는게 좋아
     String ssNumber; //주민번호
     int balance = 0;
+
+    //모든 문자를 대문자로, 여러 단어가 사용된다면 단어 사이 언더바.
+    final int COLOR_RAINBOW=7;
 
     //초기화를 위한 메소드 필요해. 이걸 생성자라고 함. 딱 한번만 써야돼.이건 자바가 자동으로 불러줘 내 편의를 위해
     public BankAccount(String acc, String ss, int bal){//생성자다!
