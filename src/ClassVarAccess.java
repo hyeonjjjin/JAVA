29211d0b8ebc83e7b719f73bfbce1cf6d447ad8f
hjjja <jahj30734@gmail.com>
2021-02-20 23:38:04 +0900

    chapter10-2_fin

--- src/ClassVarAccess.java
+++ src/ClassVarAccess.java
@@ -1,4 +1,4 @@
-//chapter 10-1
+//chapter 10-1, 10-2
 //PI 처럼 바뀔 일 없고 어디에서나 일정한 값을 갖는 변수의 경우 static으로 사용하면 좋다. 대신 final 형태로!!
 class AccessWay{
     static int num =0;
@@ -17,9 +17,24 @@ class InstCnt{
     }
 }
 
+class NumberPrinter {
+    private int myNum=0;
+    static void showInt(int n){System.out.println(n);}
+    static void showDouble(double n){System.out.println(n);}
+
+    void setMyNumber(int n){myNum = n;}
+    void showMyNumber(){showInt(myNum);}
+}
+
+class SimpleCalculator{
+    static final double PI =3.1415;
+    static double add(double n1,double n2) {return n1+n2;}
+    static double min(double n1,double n2) {return n1-n2;}
+}
 
 public class ClassVarAccess {
     public static void main(String[] args){
+        //10-1
         AccessWay way = new AccessWay();//여기서 num==1
         way.num++;//여기서 ==2
         System.out.println("num: "+way.num);
@@ -28,6 +43,9 @@ public class ClassVarAccess {
 
         InstCnt.instNum -=15;
         System.out.println("인스턴스 선언 없이 값 접근만 했다면?! 계산된다?? : "+InstCnt.instNum);//초기화 되고 계산된다~
+
+        //10-2
+        NumberPrinter.showInt(20);//인스턴스 생성해서 접근하는 것 보다 이게 더 알아보기 좋음
 
     }
 }
